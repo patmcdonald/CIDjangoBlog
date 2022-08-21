2920d62fc0574e77bbb85c3004f8e0f3b2961bb1
Pat McDonald <patmcdonald@me.com>
2022-08-21 21:06:02 +0000

    models.py 32

--- blog/models.py
+++ blog/models.py
@@ -29,7 +29,6 @@ class Post(models.Model):
 
 # Comment model
 class Comment(models.Model):
-    
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
