c0ef0b234edf53b656d8a56ea9a1895562b97454
Pat McDonald <patmcdonald@me.com>
2022-08-27 14:41:45 +0000

    add template files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(statue=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
+
+
